205bbd9c326d849aeacee9ba404bfc3477723651
Ben Artin <ben@artins.org>
2019-12-04 00:21:00 -0500

    Override default behavior of system2 wrt newlines on Windows

--- R/docknitr.R
+++ R/docknitr.R
@@ -12,14 +12,9 @@ docker_engine = function(options) {
   }
   input = options$code
 
-  # Newline conversion -- NA = leave alone, default = "\n" (UNIX newlines)
-  if (is.null(options$newline)) {
-    options$newline = "\n"
-  }
-  if (!is.na(options$newline)) {
-    input = stringr::str_replace(input, "\r\n", "\n") # Windows -> Unix
-    input = stringr::str_replace(input, "\r", "\n") # Old Mac -> Unix
-    input = stringr::str_replace(input, "\n", options$newline) # Unix -> desired
+  # Newline conversion -- NA = leave alone, NULL = default = "\n" (UNIX newlines)
+  if (is.null(options$input.sep)) {
+    options$input.sep = "\n"
   }
 
   # Run docker if options$eval
@@ -27,7 +22,7 @@ docker_engine = function(options) {
   output = ""
   if (options$eval) {
     message(sprintf('running: %s %s', command, paste0(params, collapse=" ")))
-    result = system2(command, shQuote(params), stdout = outputFile, stderr = outputFile, input = input, env = options$engine.env)
+    result = system2.sep(command, shQuote(params), stdout = outputFile, stderr = outputFile, input = input, env = options$engine.env, sep=options$input.sep)
     output = readLines(outputFile)
     if (result != 0) {
       message = sprintf('Error in running command %s %s: %s', command, paste0(params, collapse=" "), paste0(output, collapse="\n"))
@@ -68,3 +63,67 @@ docker_alias = function(name, ...) {
   knitr::knit_engines$set(docker=docker_engine)
 }
 
+# This is the built-in system2, with sep argument added
+system2.sep = function (command, args = character(), stdout = "", stderr = "",
+          stdin = "", input = NULL, env = character(), wait = TRUE,
+          minimized = FALSE, invisible = TRUE, timeout = 0, sep = NA)
+{
+  if (!missing(minimized) || !missing(invisible))
+    message("arguments 'minimized' and 'invisible' are for Windows only")
+  if (!is.logical(wait) || is.na(wait))
+    stop("'wait' must be TRUE or FALSE")
+  intern <- FALSE
+  command <- paste(c(env, shQuote(command), args), collapse = " ")
+  if (is.null(stdout))
+    stdout <- FALSE
+  if (is.null(stderr))
+    stderr <- FALSE
+  else if (isTRUE(stderr)) {
+    if (!isTRUE(stdout))
+      warning("setting stdout = TRUE")
+    stdout <- TRUE
+  }
+  if (identical(stdout, FALSE))
+    command <- paste(command, ">/dev/null")
+  else if (isTRUE(stdout))
+    intern <- TRUE
+  else if (is.character(stdout)) {
+    if (length(stdout) != 1L)
+      stop("'stdout' must be of length 1")
+    if (nzchar(stdout)) {
+      command <- if (identical(stdout, stderr))
+        paste(command, ">", shQuote(stdout), "2>&1")
+      else paste(command, ">", shQuote(stdout))
+    }
+  }
+  if (identical(stderr, FALSE))
+    command <- paste(command, "2>/dev/null")
+  else if (isTRUE(stderr)) {
+    command <- paste(command, "2>&1")
+  }
+  else if (is.character(stderr)) {
+    if (length(stderr) != 1L)
+      stop("'stderr' must be of length 1")
+    if (nzchar(stderr) && !identical(stdout, stderr))
+      command <- paste(command, "2>", shQuote(stderr))
+  }
+  if (!is.null(input)) {
+    if (!is.character(input))
+      stop("'input' must be a character vector or 'NULL'")
+    f <- tempfile()
+    on.exit(unlink(f))
+    if (!is.na(sep)) {
+      con <- file(f, "w+b")
+      writeLines(input, con, sep=sep)
+      close(con)
+    } else {
+      writeLines(input, f)
+    }
+    command <- paste(command, "<", shQuote(f))
+  }
+  else if (nzchar(stdin))
+    command <- paste(command, "<", stdin)
+  if (!wait && !intern)
+    command <- paste(command, "&")
+  system(command, intern=intern, timeout=timeout)
+}
