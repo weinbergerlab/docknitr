6e3552c444565fa97f5cc57fbcf092666f0f842e
Ben Artin <ben@artins.org>
2019-12-04 15:54:18 -0500

    Use exec_wait instead of system2 because it has better redirect semantics

--- R/docknitr.R
+++ R/docknitr.R
@@ -18,11 +18,22 @@ docker_engine = function(options) {
   }
 
   # Run docker if options$eval
+  inputFile = tempfile()
+  on.exit(unlink(inputFile))
+  if (!is.na(options$input.sep)) {
+    con <- file(inputFile, "w+b")
+    writeLines(input, con, sep=options$input.sep)
+    close(con)
+  } else {
+    writeLines(input, inputFile)
+  }
+
   outputFile = tempfile()
+  on.exit(unlink(outputFile))
   output = ""
   if (options$eval) {
     message(sprintf('running: %s %s', command, paste0(params, collapse=" ")))
-    result = system2.sep(command, shQuote(params), stdout = outputFile, stderr = outputFile, input = input, env = options$engine.env, sep=options$input.sep)
+    result = sys::exec_wait(command, params, std_out = outputFile, std_err = outputFile, std_in = inputFile)
     output = readLines(outputFile)
     if (result != 0) {
       message = sprintf('Error in running command %s %s: %s', command, paste0(params, collapse=" "), paste0(output, collapse="\n"))
@@ -63,67 +74,3 @@ docker_alias = function(name, ...) {
   knitr::knit_engines$set(docker=docker_engine)
 }
 
-# This is the built-in system2, with sep argument added
-system2.sep = function (command, args = character(), stdout = "", stderr = "",
-          stdin = "", input = NULL, env = character(), wait = TRUE,
-          minimized = FALSE, invisible = TRUE, timeout = 0, sep = NA)
-{
-  if (!missing(minimized) || !missing(invisible))
-    message("arguments 'minimized' and 'invisible' are for Windows only")
-  if (!is.logical(wait) || is.na(wait))
-    stop("'wait' must be TRUE or FALSE")
-  intern <- FALSE
-  command <- paste(c(env, shQuote(command), args), collapse = " ")
-  if (is.null(stdout))
-    stdout <- FALSE
-  if (is.null(stderr))
-    stderr <- FALSE
-  else if (isTRUE(stderr)) {
-    if (!isTRUE(stdout))
-      warning("setting stdout = TRUE")
-    stdout <- TRUE
-  }
-  if (identical(stdout, FALSE))
-    command <- paste(command, ">/dev/null")
-  else if (isTRUE(stdout))
-    intern <- TRUE
-  else if (is.character(stdout)) {
-    if (length(stdout) != 1L)
-      stop("'stdout' must be of length 1")
-    if (nzchar(stdout)) {
-      command <- if (identical(stdout, stderr))
-        paste(command, ">", shQuote(stdout), "2>&1")
-      else paste(command, ">", shQuote(stdout))
-    }
-  }
-  if (identical(stderr, FALSE))
-    command <- paste(command, "2>/dev/null")
-  else if (isTRUE(stderr)) {
-    command <- paste(command, "2>&1")
-  }
-  else if (is.character(stderr)) {
-    if (length(stderr) != 1L)
-      stop("'stderr' must be of length 1")
-    if (nzchar(stderr) && !identical(stdout, stderr))
-      command <- paste(command, "2>", shQuote(stderr))
-  }
-  if (!is.null(input)) {
-    if (!is.character(input))
-      stop("'input' must be a character vector or 'NULL'")
-    f <- tempfile()
-    on.exit(unlink(f))
-    if (!is.na(sep)) {
-      con <- file(f, "w+b")
-      writeLines(input, con, sep=sep)
-      close(con)
-    } else {
-      writeLines(input, f)
-    }
-    command <- paste(command, "<", shQuote(f))
-  }
-  else if (nzchar(stdin))
-    command <- paste(command, "<", stdin)
-  if (!wait && !intern)
-    command <- paste(command, "&")
-  system(command, intern=intern, timeout=timeout)
-}
